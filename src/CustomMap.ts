d98075114778cd8c27f53b7377f476075c97ddfc
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-04 16:40:00 +0000

    restricting access with interfaces

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,29 +1,42 @@
-import { User} from './User';
-import { Company} from './Company';
+import { User } from "./User";
+import { Company } from "./Company";
 
+// instructions to every other class
+// on how they can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 export class CustomMap {
-    private googleMap: google.maps.Map;
+  private googleMap: google.maps.Map;
 
-    constructor(divId: string) {
-        this.googleMap = new google.maps.Map(document.getElementById(divId), {zoom: 1, center: {
-    lat: 0,
-    lng: 0
-} });
-    }
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
 
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+  }
 
-    addMarker(mappable: User | Company): void{
-        new google.maps.Marker({map: this.googleMap, position: {
-            lat: mappable.location.lat,
-            lng: mappable.location.lng
-        }})
-    }
+  // addUserCompany(company: Company): void{
+  //     new google.maps.Marker({map: this.googleMap, position: {
+  //         lat: company.location.lat,
+  //         lng: company.location.lng
+  //     }})
 
-    // addUserCompany(company: Company): void{
-    //     new google.maps.Marker({map: this.googleMap, position: {
-    //         lat: company.location.lat,
-    //         lng: company.location.lng
-    //     }})
-
-    // }
+  // }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-import { User} from './User';
-import { Company} from './Company';
-import { CustomMap} from './CustomMap';
+import { User } from "./User";
+import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 
 const user = new User();
 const company = new Company();
 
-const customMap = new CustomMap('map')
+const customMap = new CustomMap("map");
 
-customMap.addMarker(user)
-customMap.addMarker(company)
-
- 
+customMap.addMarker(user);
+customMap.addMarker(company);

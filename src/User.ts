c59c743f39136e3be62eb3c04cb0a5d7e8fae13b
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-04 17:00:25 +0000

    optional implements clauses

--- src/User.ts
+++ src/User.ts
@@ -1,11 +1,13 @@
+import { Mappable } from "./CustomMap";
 import faker from "faker";
 
-export class User {
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string = "red";
 
   constructor() {
     this.name = faker.name.firstName();
